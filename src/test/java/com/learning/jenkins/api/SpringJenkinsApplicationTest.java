ca7e6b588da8107fc4f7ffe9aa904c7c6dfb5fdd
siddharthjain-02 <jain.siddharth0893@gmail.com>
2024-03-31 23:28:24 +0530

    second commit

--- src/test/java/com/learning/jenkins/api/SpringJenkinsApplicationTest.java
+++ src/test/java/com/learning/jenkins/api/SpringJenkinsApplicationTest.java
@@ -16,6 +16,7 @@ public class SpringJenkinsApplicationTest {
 
     @Test
     public void contextLoads(){
+        logger.info("Test case executing...");
         assertEquals(true, true);
     }
 }
